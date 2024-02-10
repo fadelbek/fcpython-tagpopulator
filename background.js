9c3a41c7473999729fccab9be444b188e2888f5d
Fadi El Bekkouri <fadi.bekkouri@gmail.com>
2024-02-10 14:07:17 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
 function fetchData(){
 
-    const urlkings = "https://script.google.com/macros/s/AKfycbxsAvlRm936bRzRWFqU9yInHqqZU2HM_rs2RT7C_BYQp0nz-ICvd6n7fTJjl6HJBzsGzQ/exec";
-    const urlqueens = "https://script.google.com/macros/s/AKfycbwIhXA9Ty9B58ue5IyWcCGV5FIovJf5iJt5cMsN6jamzwEtdaliPC75cPf_3U1_2LaQNQ/exec";
+    //const urlkingslocal = "https://script.google.com/macros/s/AKfycbxsAvlRm936bRzRWFqU9yInHqqZU2HM_rs2RT7C_BYQp0nz-ICvd6n7fTJjl6HJBzsGzQ/exec";
+    //const urlqueenslocal = "https://script.google.com/macros/s/AKfycbwIhXA9Ty9B58ue5IyWcCGV5FIovJf5iJt5cMsN6jamzwEtdaliPC75cPf_3U1_2LaQNQ/exec";
+
+    const urlkings = "https://script.google.com/macros/s/AKfycbzlcNhbTRc2vkyjzNt2JzWv4Ph0if4DhwoCYPIkz40HFNfONko_WlBPK-LwXHmpJBLL/exec"
+    const urlqueens = "https://script.google.com/macros/s/AKfycbxh0u-Za6t5TLgyIotYE6w4obCWuQ0ItWGabfNBUbcIWvT2_5fpqdSQ9TAsA3mBP4IA/exec"
 
     console.log("Access fetchData FCT");
     fetch(urlkings)
